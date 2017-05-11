404d5f3a463db4a9127d5a1d78c58c253c0bfe7b
ezekielchentnik <zeek@ezekielchentnik.com>
2017-05-11 14:50:33 -0500

    force sourcemap

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ export default function uglify (options = {}) {
 	return {
 		name: 'uglify',
 		transformBundle (code) {
-			const result = minify(code, options)
+			const result = minify(
+				code,
+				Object.assign({ sourceMap: { url: 'out.js.map' } }, options ) // force sourcemap creation
+			)
 			if (result.map) {
 				const commentPos = result.code.lastIndexOf('//#')
 				result.code = result.code.slice(0, commentPos).trim()
